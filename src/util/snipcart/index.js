b8f9c1dae3bdfe43392704e583f7eaacb52bf1ca
Regis Philibert <login@regisphilibert.com>
2023-07-20 16:06:14 -0400

    Add body excerpt to snipcart button

--- src/util/snipcart/index.js
+++ src/util/snipcart/index.js
@@ -1,4 +1,5 @@
 import { getImageURL } from '@util/sanity/image'
+import { getExcerpt } from '@util/helpers'
 const convertOuncesToGrams = (ounces) => {
   return ounces * 28.3495;
 }
@@ -11,8 +12,9 @@ const prefixObjectKeys = (object, prefix) => {
   return output
 }
 
-export const getData = ({ title, url, id = null, image, description, categories, sales_data}) => {
+export const getData = ({ title, url, id = null, image, bodyText, categories, sales_data}) => {
   const { shipping_weight, price, sku } = sales_data
+  const description = bodyText ? getExcerpt(bodyText) : false
   return prefixObjectKeys({
     name: title,
     url,
@@ -20,7 +22,7 @@ export const getData = ({ title, url, id = null, image, description, categories,
     id: sku,
     image: image ? getImageURL(image) : false,
     price,
-    weight: shipping_weight ? convertOuncesToGrams(shipping_weight) : 300,
+    weight: shipping_weight ? shipping_weight : 300,
     description,
     //taxes: ["TAX-001"]
   }, 'data-item-') 
