6f4451cf29dd9b53917abf45bdb166fd7b66e8e5
Regis Philibert <login@regisphilibert.com>
2025-03-26 10:31:04 -0400

    Update netlify function import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig, envField } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import netlify from "@astrojs/netlify/functions";
+import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.cocina214.com',
